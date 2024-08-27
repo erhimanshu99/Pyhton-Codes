079d0a746f42184a3bab87ec4cbbfa47dfd2d051
Himanshu Kumar <erhimanshu799@gmail.com>
2024-08-27 23:08:35 +0530

    initial commit

--- Himanshu/ashutosh age.py
+++
@@ -1,4 +0,0 @@
-h= int(input("enter the height"))
-w= int(input("enter the weight"))
-age= w/h
-print(age)
---
+++ Operators/ashutosh age.py
@@ -0,0 +1,4 @@
+h= int(input("enter the height")) #change1
+w= int(input("enter the weight"))
+age= w/h 
+print(age)
